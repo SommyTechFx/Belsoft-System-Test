3e0bb1b3974fbd2fb35c17ca6c866203fcbafac4
Aningwu chisom <152747011+SommyTechFx@users.noreply.github.com>
2024-12-04 22:25:12 +0100

    what we are section done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./components/Sections/Header";
 import Hero from "./components/Sections/Hero";
+import WHAT_WE_DO from "./components/Sections/WHAT_WE_DO";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Hero />
+      <WHAT_WE_DO />
     </div>
   );
 }
